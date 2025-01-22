d6b30225201e555834cc899a78214d2735708430
Michael Valdez <mikeyvaldez92@yahoo.com>
2025-01-22 14:58:31 -0800

    select distinct

---
+++ first_bit/SelectDistinct/selectdistinct.sql
@@ -0,0 +1,11 @@
+
+
+
+
+-- This query is based on if you have a table named payment
+-- and it consists of rows and columns of the following data
+
+SELECT DISTINCT
+amount
+FROM payment
+ORDER BY amount DESC
