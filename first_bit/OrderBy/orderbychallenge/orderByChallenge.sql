0b482d33dc5c04c52b34848a5e21fd7c05d4e208
Michael Valdez <mikeyvaldez92@yahoo.com>
2025-01-22 14:50:12 -0800

    first bit

---
+++ first_bit/OrderBy/orderbychallenge/orderByChallenge.sql
@@ -0,0 +1,18 @@
+
+
+
+-- This is a simple exercise using the 'SELECT' and 'ORDER BY'query.
+-- along with the 'DESC' command which stands for descending.
+-- The png file shows what you are supposed to perform.
+
+SELECT
+first_name,
+last_name,
+email
+FROM customer
+ORDER BY last_name DESC, first_name DESC
+
+
+
+
+
---
+++ first_bit/OrderBy/selectandorderby/selectAndOrderBy.sql
@@ -0,0 +1,11 @@
+
+
+-- This is a simple exercise using the 'SELECT' and 'ORDER BY' query.
+-- along with the 'DESC' command which stands for descending.
+-- The png file shows what you are supposed to perform.
+
+
+SELECT
+*
+FROM books
+ORDER BY price DESC
