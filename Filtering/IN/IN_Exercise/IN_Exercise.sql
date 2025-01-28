6d0f2515a002b32238e47350d5a2f006f1bc1e70
Michael Valdez <mikeyvaldez92@yahoo.com>
2025-01-27 16:10:35 -0800

    IN exercise

---
+++ Filtering/IN/IN_Exercise/IN_Exercise.sql
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+-- This query is related to the png file
+SELECT * FROM payment
+WHERE customer_id IN (12,25,67,93,124,234)
+AND amount IN (4.99,7.99,9.99)
+AND payment_date BETWEEN '2020-01-01' AND '2020-02-01'
